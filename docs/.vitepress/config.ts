7c7bbff311ef1644e0de4283ed8fde853cb7f2bf
Jessé Lins <jesse.lins@buser.com.br>
2023-04-19 12:10:08 -0300

    Adjust base url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: 'https://linspw.github.io/Easy-DnD/',
   title: "Vue-Easy-DnD",
   description: "A HTML5 drag-and-drop replacement",
   themeConfig: {
